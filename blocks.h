a6cd678c0fbc328bfe60a056cfa562a9469e0f6a
Drew Herron <herron@mailbox.org>
2023-03-16 23:03:03 -0700

    Battery indicator for Gentoo

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"B:", "cat /sys/class/power_supply/BAT1/capacity" , 30  , 0} ,
+    {"B:", "cat /sys/class/power_supply/BAT0/capacity" , 30  , 0} ,
 
 	{"M:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
     
